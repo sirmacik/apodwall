4769c2948bbae57c7f9a82132f3f66bc7e9b1d66
sirmacik <sirmacik@openmailbox.org>
2015-09-05 14:40:15 +0200

    Removed unused imports, improved style

--- apodwall.py
+++ apodwall.py
@@ -17,12 +17,11 @@
 # You should have received a copy of the GNU General Public License
 # along with this program. If not, see <http://www.gnu.org/licenses/>.
 
-#import sys
+# import sys
 import os
 import time
 from pattern.web import download, plaintext
 from bs4 import BeautifulSoup
-import Tkinter as tk
 from PIL import Image, ImageFont, ImageDraw, ImageFile
 import textwrap
 import subprocess
@@ -41,6 +40,7 @@ os.chdir(directory)
 # Make it work in some cases
 ImageFile.LOAD_TRUNCATED_IMAGES = True
 
+
 # Get information about the main screen
 def get_screensizecmd():
     command = ["xrandr", "--current", "--screen", "0"]
@@ -49,15 +49,17 @@ def get_screensizecmd():
                          stderr=subprocess.STDOUT)
     return iter(p.stdout.readline, b'')
 
+
 def get_screensize():
-    i = 0    
+    i = 0
     for line in get_screensizecmd():
         if i == 0:
             i += 1
         elif i == 1:
             str(line)
             # possible output:
-            #     LVDS1 connected primary 1366x768+0+0 (normal left inverted right x axis y axis) 256mm x 144mm
+            #     LVDS1 connected primary 1366x768+0+0 (normal left inverted
+            #     right x axis y axis) 256mm x 144mm
             # so we need to parse it
             line = line.split()
 
@@ -91,27 +93,29 @@ html = download(website+"astropix.html", cached=False, unicode=True)
 
 soup = BeautifulSoup(html, "html.parser")
 
+
 def get_explanation(soup):
     explanation = soup.find("b", string=" Explanation: ")
     explanation = str(explanation.parent)
-    
+
     explanation2 = BeautifulSoup(explanation, "html.parser")
     explanation2 = explanation2.p.center
     explanation2 = str(explanation2)
-    
+
     text = explanation.replace(explanation2, "")
     text = plaintext(text, linebreaks=2, indentation=True)
     text = str(text).replace("\n", "")
     text = textwrap.wrap(text, width=140)
     return text
-    
+
+
 def get_image():
     img = soup.img.parent.get('href')
 
     w = screen_width
-    h = screen_height
+    # h = screen_height
 
-    img = website+img                           
+    img = website+img
     imgdw = download(img, cached=False, unicode=False)
     imgname = timestamp+"_apod.jpg"
     imgfile = open(imgname, "wb")
@@ -121,7 +125,7 @@ def get_image():
     wall = Image.open(imgname)
     draw = ImageDraw.Draw(wall)
     FOREGROUND = (255, 255, 255)
-    #font = ImageFont.truetype("sans-serif.ttf", 16)
+    # font = ImageFont.truetype("sans-serif.ttf", 16)
     font_path = "/usr/share/fonts/truetype/freefont/FreeSans.ttf"
     font = ImageFont.truetype(font_path, 20, encoding='unic')
     y_text = 0
@@ -134,17 +138,20 @@ def get_image():
         draw.text(((w - width) / 2, y_text), line, font=font, fill=FOREGROUND)
         y_text += height
 
-        #draw.text((0, 0),text,(255,255,255))
+        # draw.text((0, 0),text,(255,255,255))
     wall.save(imgname)
-    
+
     imgpath = directory + "/" + imgname
     uri = "'file://%s'" % imgpath
     return uri
 
+
 def set_wall(uri):
-    args = ["gsettings", "set", "org.gnome.desktop.background", "picture-uri", uri]
-    opts = ["gsettings", "set", "org.gnome.desktop.background", "picture-options", "stretched"]
-    
+    args = ["gsettings", "set", "org.gnome.desktop.background",
+            "picture-uri", uri]
+    opts = ["gsettings", "set", "org.gnome.desktop.background",
+            "picture-options", "scaled"]
+
     subprocess.Popen(opts)
     subprocess.Popen(args)
 
@@ -152,5 +159,3 @@ def set_wall(uri):
 text = get_explanation(soup)
 uri = get_image()
 set_wall(uri)
-
-#print img
